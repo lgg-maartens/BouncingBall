89827c51ae580034e47f775f9a3c65a48eedb12d
Tim Logtenberg <45438244+lgg-maartens@users.noreply.github.com>
2021-09-08 08:57:57 +0000

    start: bouncing ball

--- script.js
+++ script.js
@@ -1,20 +1,27 @@
-var x, y;
+var x, y, vx, vy;
 
 function setup() {
 	createCanvas(500, 400);
 
-  x = 0;
+  x = 30;
   y = 200;
+  vx = 5;
+  vy = 5;
 }
 
 function draw() {
 	background(225);
   
   ellipse(x,y,50,50);
-  x = x + 5;
+  x = x + vx;
+  y = y + vy;
 
-  if(x > 500){
-    x = 0;
+  if(x < 25 || x >= 475){
+    vx = vx * -1;
+  }
+
+  if(y < 25 || y >= 375){
+    vy = vy * -1;
   }
 }
 
