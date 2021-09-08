addd3ca72b9b7bea2e8ae9d08a9842038158708e
Tim Logtenberg <45438244+lgg-maartens@users.noreply.github.com>
2021-09-08 10:57:40 +0000

    added a ball class and made 3 bouncy balls

--- script.js
+++ script.js
@@ -1,27 +1,46 @@
-var x, y, vx, vy;
+class Ball{
+  constructor(x, y, w, h, vx, vy) {
+    this.x = x;
+    this.y = y;
+    this.width = w;
+    this.height = h;
+    this.vx = vx;
+    this.vy = vy;
+  }
+
+  draw(){
+    ellipse(this.x,this.y,50,50);
+    this.x = this.x + this.vx;
+    this.y = this.y + this.vy;
+
+    if(this.x < 25 || this.x >= 475){
+      this.vx = this.vx * -1;
+    }
+
+    if(this.y < 25 || this.y >= 375){
+      this.vy = this.vy * -1;
+    }
+  }
+}
+
+var ball1, ball2, ball3;
 
 function setup() {
 	createCanvas(500, 400);
 
-  x = 30;
-  y = 200;
-  vx = 5;
-  vy = 5;
+  ball1 = new Ball(30,200,50,50,5,5);
+
+  ball2 = new Ball(100,100,50,50,10,10);
+
+  ball3 = new Ball(200,150,50,50,1,-1);
+
 }
 
 function draw() {
-	background(225);
+	background(225);  
   
-  ellipse(x,y,50,50);
-  x = x + vx;
-  y = y + vy;
-
-  if(x < 25 || x >= 475){
-    vx = vx * -1;
-  }
-
-  if(y < 25 || y >= 375){
-    vy = vy * -1;
-  }
+  ball1.draw();
+  ball2.draw();
+  ball3.draw();
 }
 
