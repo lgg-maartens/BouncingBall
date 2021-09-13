94eb54082b3337b539cc0db3022269d3f5e248e8
Tim Logtenberg <45438244+lgg-maartens@users.noreply.github.com>
2021-09-13 10:55:33 +0000

    Added color

--- script.js
+++ script.js
@@ -1,38 +1,42 @@
 class Ball{
-  constructor(x, y, w, h, vx, vy) {
+  
+  constructor(x, y, w, h, vx, vy,_color) {
     this.x = x;
     this.y = y;
     this.width = w;
     this.height = h;
     this.vx = vx;
     this.vy = vy;
+    this.color = _color;
   }
 
   draw(){
+    fill(this.color);
     ellipse(this.x,this.y,50,50);
     this.x = this.x + this.vx;
     this.y = this.y + this.vy;
 
-    if(this.x < 25 || this.x >= 475){
+    if(this.x < 25 || this.x >= 375){
       this.vx = this.vx * -1;
     }
 
-    if(this.y < 25 || this.y >= 375){
+    if(this.y < 25 || this.y >= 275){
       this.vy = this.vy * -1;
     }
   }
 }
 
+
 var ball1, ball2, ball3;
 
 function setup() {
-	createCanvas(500, 400);
+	createCanvas(400, 300);
 
-  ball1 = new Ball(30,200,50,50,5,5);
+  ball1 = new Ball(30,200,50,50,5,5,"red");
 
-  ball2 = new Ball(100,100,50,50,10,10);
+  ball2 = new Ball(120,210,50,50,3,3,"blue");
 
-  ball3 = new Ball(200,150,50,50,1,-1);
+  ball3 = new Ball(200,150,50,50,1,1,"green");
 
 }
 
